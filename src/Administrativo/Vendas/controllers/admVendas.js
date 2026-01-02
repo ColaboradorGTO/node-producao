a34393a28a97f2f0150b8065699ef97a2ff8a021
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-02 17:36:19 -0300

    refactor:  vercel

--- src/Administrativo/Vendas/controllers/admVendas.js
+++ src/Administrativo/Vendas/controllers/admVendas.js
@@ -492,6 +492,8 @@ class AdmVendasControllers {
             try {
                 const apiUrl = `${url}/api/administrativo/resumo-venda.xsjs?idEmpresa=${idEmpresa}&dataPesquisa=${dataPesquisa}`;
                 const response = await axios.get(apiUrl);
+                console.log(response.data, 'response.data');
+                console.log(response, 'response');
                 return res.json(response.data);
             } catch (error) {
                 console.error("Erro ao conectar ao servidor:", error);
